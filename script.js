e4a0834ee822fece4ac06e9673ce012cd8dc3755
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-22 22:12:35 +0100

    - Fix humanScore which was not updating - Print game result and final score to the console - Add a loop to getHumanChoice to ensure an invalid choice does not skip the round

--- script.js
+++ script.js
@@ -12,39 +12,34 @@ function getComputerChoice () { // returns computer choice
     }
 }
 
-function getHumanChoice () { // returns human choice. If invalid, calls itself again
-    let humanChoice = prompt("Enter your choice").toLowerCase();
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        // console.log(humanChoice);
-        return humanChoice;
+function getHumanChoice () { // Gets and returns human choice. 
+    let humanChoice = prompt("Enter your choice").toLowerCase(); // Get the choice from the player
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") { //If the input is invalid, prompt again
+        console.log("Invalid choice, enter 'rock', 'paper' or 'scissors'"); //Inform the player the choice is invalid
+        humanChoice = prompt("Enter your choice").toLowerCase(); //Try to get a new choice
     }
-    else {
-        console.log("Invalid choice, enter 'rock', 'paper' or 'scissors'");
-        // console.log(humanChoice);
-        getHumanChoice();
-    }
+    return humanChoice;
 }
 
 function playRound (humanChoice, computerChoice) { // play a round of the game. Returns score increment
-    let humanScoreRound =0, computerScoreRound = 0;
     if (humanChoice === computerChoice){
         console.log(`It's a tie! ${humanChoice} equals ${computerChoice}`);
+        return "tie";
     }
     else if (humanChoice === "rock" && computerChoice === "scissors"
     || humanChoice === "paper" && computerChoice === "rock"
     || humanChoice === "scissors" && computerChoice === "paper"
     ) {
         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScoreRound++;
+        return "win";
     }
     else if (computerChoice === "rock" && humanChoice === "scissors"
     || computerChoice === "paper" && humanChoice === "rock"
     || computerChoice === "scissors" && humanChoice === "paper"
     ) {
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScoreRound++;
     }
-    return humanScoreRound, computerScoreRound;
+    return "lose";
 }
 
 
@@ -53,14 +48,31 @@ function playRound (humanChoice, computerChoice) { // play a round of the game. 
 
 function playGame () { // play 5 rounds of the game
     let humanScore = 0, computerScore = 0;
-    let humanScoreIncrement = 0, computerScoreIncrement = 0;
     for (let game_counter = 0; game_counter < 5; game_counter++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        humanScoreIncrement, computerScoreIncrement = playRound(humanSelection, computerSelection);
-        humanScore += humanScoreIncrement;
-        computerScore += computerScoreIncrement;
+        let humanSelection = getHumanChoice(); // Get human choice
+        let computerSelection = getComputerChoice(); // Get computer choice
+        let result = playRound(humanSelection, computerSelection); //Play a round and return the result
+        switch (result) { // Increment player or computer score based on result
+            case "win":
+                humanScore++;
+                break;
+            case "lose":
+                computerScore++;
+                break;
+            case "tie":
+                break;
+        }
         console.log(`Human score: ${humanScore}, Computer score: ${computerScore}`);
+    }
+    // Print the result of the game
+    if (humanScore > computerScore) {
+        console.log(`You win the game! ${humanScore} to ${computerScore}`);
+    }
+    else if (humanScore < computerScore) {
+        console.log(`You lose the game! ${humanScore} to ${computerScore}`);
+    }
+    else {
+        console.log(`It's a tie! ${humanScore} to ${computerScore}`);
     }
 }
 
