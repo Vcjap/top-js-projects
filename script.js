4c130bde4ebfe31bacd788a498d9518f0dacb2b4
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-22 21:34:21 +0100

    Create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let humanScore, computerScore = 0;
+
 function getComputerChoice () {
     let choiceNumber = Math.floor(Math.random()*3);
     switch (choiceNumber) {
@@ -11,7 +13,7 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt("Enter your choice")
+    let humanChoice = prompt("Enter your choice").toLowerCase();
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
         // console.log(humanChoice);
         return humanChoice;
@@ -22,3 +24,26 @@ function getHumanChoice () {
         getHumanChoice();
     }
 }
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice){
+        console.log(`It's a tie! ${humanChoice} equals ${computerChoice}`);
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"
+    || humanChoice === "paper" && computerChoice === "rock"
+    || humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice === "rock" && humanChoice === "scissors"
+    || computerChoice === "paper" && humanChoice === "rock"
+    || computerChoice === "scissors" && humanChoice === "paper"
+    ) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
