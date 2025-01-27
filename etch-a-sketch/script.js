e8a3217e21ee2585711d1aa7d3c2ad70e44f5aff
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-27 21:36:36 +0100

    Refactor createGrid to accept any size

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,17 +1,37 @@
-// Create grid
-mainDiv = document.querySelector("#mainDiv");
-for (i=0; i<16; i++) {
-    const divL1 = document.createElement("div");
-    divL1.id = `level1-${i}`;
-    divL1.className = "l1"
-    for (j=0; j<16; j++) {
-        const divL2 = document.createElement("div");
-        divL2.id = `level2-${j}`;
-        divL2.className = "l2";
-        divL1.appendChild(divL2);
+function createGrid(gridSize) {
+    for (i=0; i<gridSize; i++) {
+        const divL1 = document.createElement("div");
+        divL1.id = `level1-${i}`;
+        divL1.className = "l1"
+        for (j=0; j<gridSize; j++) {
+            const divL2 = document.createElement("div");
+            divL2.id = `level2-${j}`;
+            divL2.className = "l2";
+            divL1.appendChild(divL2);
+        }
+        mainDiv.appendChild(divL1);
     }
-    mainDiv.appendChild(divL1);
 }
+
+function getGridSize () {
+    let gridSize = prompt("Enter the new grid size. Only enter numbers lower than or equal to 100!")
+    while (!Number.isInteger(gridSize) || gridSize > 100) {
+        gridSize = prompt("Enter the new grid size. Only enter numbers lower than or equal to 100!")
+    }
+    return gridSize;
+}
+
+function deleteGrid () {
+    grid = document.querySelectorAll(".l1");
+    grid.forEach((cell)=> {
+        cell.remove();
+    });
+}
+
+// Create initial grid
+mainDiv = document.querySelector("#mainDiv");
+const firstGridNum = 16
+createGrid(firstGridNum);
 
 // Add a class when the mouse enters the cell
 cells = document.querySelectorAll(".l2");
@@ -27,11 +47,3 @@ cells.forEach((cell) => {
         cell.classList.remove("hoverEffect");
     })
 });
-
-function getGridSize () {
-    let gridSize = prompt("Enter the new grid size. Only enter numbers lower than or equal to 100!")
-    while (!Number.isInteger(gridSize) || gridSize > 100) {
-        gridSize = prompt("Enter the new grid size. Only enter numbers lower than or equal to 100!")
-    }
-    return gridSize;
-}
