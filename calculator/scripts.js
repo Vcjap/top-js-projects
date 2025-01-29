2480ee38c93b6e0164312ac9e15dc312f6ec227a
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-29 12:09:30 +0100

    Add operate function

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -14,3 +14,18 @@ function multiply(n1,n2) {
 function divide(n1,n2) {
     return Math.round((n1/n2)*100)/100; // The returned number is rounded to 2 decimals
 }
+
+function operate(n1,operator,n2) {
+    switch(operator) {
+        case "+":
+            return add(n1,n2);
+        case "-":
+            return subtract(n1,n2);
+        case "x":
+            return multiply(n1,n2);
+        case "/":
+            return divide(n1,n2);
+        default:
+            return "No operation found";
+    }
+}
