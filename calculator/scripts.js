91732f679ec1ca51f9d4db7bb590efb65e891a3f
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-29 11:54:56 +0100

    Create basic functions add,subtract,multiply,divide

---
+++ calculator/scripts.js
@@ -0,0 +1,16 @@
+// Basic Functions
+function add(n1,n2) {
+    return n1+n2;
+}
+
+function subtract(n1,n2) {
+    return n1-n2;
+}
+
+function multiply(n1,n2) {
+    return n1*n2;
+}
+
+function divide(n1,n2) {
+    return Math.round((n1/n2)*100)/100; // The returned number is rounded to 2 decimals
+}
